89b26d7ed0f3af7f3dffeeb16a226f8253656097
deep_blu <79891536+deep-blu109@users.noreply.github.com>
2023-03-11 17:19:29 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     {
-      resolve: `deepBlu-ocean`,
+      resolve: `gatsby-theme-garden`,
       options: {
         contentPath: `${__dirname}/src/site/notes`,
         rootNote: `/home`,
